4e308ae7cc3edd2a146802690b3ea985b7b62f68
BlueZeeKing24 <52867955+BlueZeeKing@users.noreply.github.com>
2022-03-04 14:58:27 -0500

    add antd

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,31 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { Typography, Space } from "antd";
+const { Title, Paragraph, Text, Link } = Typography;
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Canvas Alternative</title>
-        <meta name="description" content="Alternative to Instructure's Canvas using react" />
+        <meta
+          name="description"
+          content="Alternative to Instructure's Canvas using react"
+        />
       </Head>
 
       <main>
-        <h1>Hi!</h1>
+        <Space direction="vertical" size="large" style={{padding: "1rem"}}>
+          <Typography>
+            <Title>Hi!</Title>
+            <Paragraph>
+              This is a simple alternative to Instructure's Canvas using react.
+            </Paragraph>
+          </Typography>
+        </Space>
       </main>
 
       <footer></footer>
     </div>
-  )
+  );
 }
