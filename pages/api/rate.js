366eb153b8e732aed3f880cc67a5eb8a7939842d
BlueZeeKing <52867955+BlueZeeKing@users.noreply.github.com>
2022-03-12 16:12:41 -0500

    added like

--- pages/api/rate.js
+++ pages/api/rate.js
@@ -1,23 +1,16 @@
 import axios from "axios";
 
 export default async function handler(request, response) {
-  const res = await fetch(
+
+  const res = await axios.post(
     `https://apsva.instructure.com/api/v1/courses/${request.query.course}/discussion_topics/${request.query.discussion}/entries/${request.query.item}/rating`,
     {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${request.headers.authorization}`,
-      },
-      body: `rating=${request.query.rate}&_method=POST`,
+      rating: parseInt(request.query.rate),
+    },
+    {
+      headers: { Authorization: `Bearer ${request.headers.authorization}` },
     }
   );
-  console.log(
-    `rating=${request.query.rate}&_method=POST&authenticity_token=${nanoid()}`
-  );
-  console.log(res);
-  response.setHeader(
-    "x-rate-limit-remaining",
-    res.headers.get("x-rate-limit-remaining")
-  );
+
   return response.status(res.status).send();
 }
